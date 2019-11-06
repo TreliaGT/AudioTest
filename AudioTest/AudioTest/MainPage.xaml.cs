e95cbfc52ea17752f267aee429357406b5af08bb
GT <v244682@tafe.wa.edu.au>
2019-11-06 10:19:10 +0800

    First Commit

---
+++ AudioTest/AudioTest/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace AudioTest
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+
+        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void PlayClicked(object sender, EventArgs args)
+        {
+          
+            var player = Plugin.SimpleAudioPlayer.CrossSimpleAudioPlayer.Current;
+
+      
+            player.Load(GetStreamFromFile("Sleep.mp3"));
+
+            player.Play();
+        }
+
+        public void StopClicked(object sender, EventArgs args)
+        {
+            var player = Plugin.SimpleAudioPlayer.CrossSimpleAudioPlayer.Current;
+            player.Stop();
+        }
+
+        Stream GetStreamFromFile(string filename)
+        {
+            var assembly = typeof(App).GetTypeInfo().Assembly;
+            var stream = assembly.GetManifestResourceStream("Music." + filename);
+            return stream;
+        }
+    }
+}
